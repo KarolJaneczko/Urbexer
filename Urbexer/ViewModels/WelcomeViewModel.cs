6bbc0d5c80b679a68fdbcf103a314e4d4b94bcb5
Karol Janeczko <karol.janeczko@outlook.com>
2022-02-18 19:38:55 +0100

    komit

--- Urbexer/ViewModels/WelcomeViewModel.cs
+++ Urbexer/ViewModels/WelcomeViewModel.cs
@@ -5,16 +5,8 @@ using Urbexer.Views;
 using Xamarin.Forms;
 
 namespace Urbexer.ViewModels {
-
     public class WelcomeViewModel : BaseViewModel {
-
-        public Command WelcomeSignInCommand { get; }
-
-        public WelcomeViewModel() {
-            WelcomeSignInCommand = new Command(WelcomeSignInPressed);
-        }
-
-        public async void WelcomeSignInPressed(object obj) { 
+        public WelcomeViewModel() {       
         }
     }
 }
--- Urbexer/Views/WelcomePage.xaml.cs
+++ Urbexer/Views/WelcomePage.xaml.cs
@@ -2,6 +2,8 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Xamarin.Essentials;
+using Octane.Xamarin.Forms.VideoPlayer;
+using System.IO;
 
 namespace Urbexer.Views {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -12,6 +14,17 @@ namespace Urbexer.Views {
             WelcomeLayout.WidthRequest = DeviceDisplay.MainDisplayInfo.Width / 4;
             WelcomeLayout.Padding = new Thickness(DeviceDisplay.MainDisplayInfo.Width * 0.01, DeviceDisplay.MainDisplayInfo.Width * 0.05,
                 DeviceDisplay.MainDisplayInfo.Width * 0.01, DeviceDisplay.MainDisplayInfo.Width * 0.01);
+
+            //BackgroundVideo.MinimumWidthRequest = DeviceDisplay.MainDisplayInfo.Width;
+            //BackgroundVideo.MinimumHeightRequest = DeviceDisplay.MainDisplayInfo.Height;
+            //BackgroundVideo.Source = VideoSource.FromUri("https://i.imgur.com/UIe3Oue.mp4");
+            BackgroundVideo.Source = VideoSource.FromFile("\Resources\mainvideo.mp4");
+        }
+        public void WelcomeSignInPressed(object sender, System.EventArgs e) {
+            Shell.Current.GoToAsync("//AboutPage");
+        }
+        public void WelcomeSignUpPressed(object sender, System.EventArgs e) {
+            Shell.Current.GoToAsync("//AboutPage");
         }
     }
 }
