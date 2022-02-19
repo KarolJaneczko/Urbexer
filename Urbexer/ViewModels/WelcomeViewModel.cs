b7161ccfdc6027c2c8871d9ae47c2cd3b932799f
Bartosz Jarząb <bartosz.jarzab99@gmail.com>
2022-02-19 18:14:57 +0100

    W końcu coś działa

--- Urbexer/ViewModels/WelcomeViewModel.cs
+++ Urbexer/ViewModels/WelcomeViewModel.cs
@@ -1,12 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using Urbexer.Views;
 using Xamarin.Forms;
 
-namespace Urbexer.ViewModels {
-    public class WelcomeViewModel : BaseViewModel {
-        public WelcomeViewModel() {       
+namespace Urbexer.ViewModels
+{
+
+    public class WelcomeViewModel : BaseViewModel
+    {
+
+        public Command WelcomeSignInCommand { get; }
+        public Command WelcomeRegisterCommand { get; }
+        public List<Onboarding> Onboardings { get; set; }
+
+
+        public WelcomeViewModel()
+        {
+            WelcomeSignInCommand = new Command(WelcomeSignInPressed);
+            WelcomeRegisterCommand = new Command(WelcomeRegisterPressed);
+            Onboardings = GetOnboarding();
+
+
         }
+
+        private List<Onboarding> GetOnboarding()
+        {
+            return new List<Onboarding>
+            {
+                new Onboarding {Heading = "Heading1", Caption = "Caption1"},
+                new Onboarding {Heading = "Heading2", Caption = "Caption2"},
+                new Onboarding {Heading = "Heading3", Caption = "Caption3"},
+                new Onboarding {Heading = "Heading4", Caption = "Caption4"},
+                new Onboarding {Heading = "Heading5", Caption = "Caption5"}
+                
+            };
+        }
+
+
+        public async void WelcomeSignInPressed(object obj)
+        {
+            Shell.Current.GoToAsync("//SignInPage");
+        }
+        public async void WelcomeRegisterPressed(object obj)
+        {
+
+        }
+
     }
+    public class Onboarding
+    {
+
+        public string Heading { get; set; }
+        public string Caption { get; set; }
+
+    }
+
 }
+
+
--- Urbexer/Views/SignInPage.xaml.cs
+++ Urbexer/Views/SignInPage.xaml.cs
@@ -9,11 +9,11 @@ using System.Timers;
 
 namespace Urbexer.Views {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class WelcomePage : ContentPage {
-        public WelcomePage() {
+    public partial class SignInPage : ContentPage {
+        public SignInPage() {
             InitializeComponent();
-            this.BindingContext = new WelcomeViewModel();
-            AnimatedInfoCarousel();
+            this.BindingContext = new SignInViewModel();
+          //  AnimatedInfoCarousel();
 
            //WelcomeLayout.WidthRequest = DeviceDisplay.MainDisplayInfo.Width / 4;
            // WelcomeLayout.Padding = new Thickness(DeviceDisplay.MainDisplayInfo.Width * 0.01, DeviceDisplay.MainDisplayInfo.Width * 0.05,
@@ -24,33 +24,13 @@ namespace Urbexer.Views {
             BackgroundVideo.Source = VideoSource.FromUri("https://i.imgur.com/UIe3Oue.mp4");
         }
 
-        Timer timer;
-        private void AnimatedInfoCarousel()
-        {
-            timer = new Timer(3000)
-            {
-                AutoReset = true,
-                Enabled = true
-            };
-            timer.Elapsed += (s, e) =>
-            {
-                Device.BeginInvokeOnMainThread(() =>
-                  {
-                      if (cvOnboarding.Position == 4)
-                      {
-                          cvOnboarding.Position = 0;
-                          return;
-                      }
-                      cvOnboarding.Position += 1;
-                  });
-            };
-        }
+       
 
         public void WelcomeSignInPressed(object sender, System.EventArgs e) {
-            Shell.Current.GoToAsync("//SignInPage");
+            Shell.Current.GoToAsync("//AboutPage");
         }
         public void WelcomeSignUpPressed(object sender, System.EventArgs e) {
-            Shell.Current.GoToAsync("//SignInPage");
+            Shell.Current.GoToAsync("//AboutPage");
         }
     }
 }
