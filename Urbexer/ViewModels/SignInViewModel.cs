216a8f11e5a2003f97c6d0f6af39bb13947a0682
Karol Janeczko <karol.janeczko@outlook.com>
2022-02-19 19:42:31 +0100

    chuj (ale mały)

--- Urbexer/ViewModels/SignInViewModel.cs
+++ Urbexer/ViewModels/SignInViewModel.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Urbexer.ViewModels
-{
-    internal class SignInViewModel 
-    {
+namespace Urbexer.ViewModels {
+    internal class SignInViewModel {
     }
 }
