7e0cd91e4868b4c0098b14170e6fff7b1f1ab44b
MarcinWozniak345 <97433864+MarcinWozniak345@users.noreply.github.com>
2022-02-20 14:54:43 +0100

    gotowe logowanie w API
    akcje rejestracji, potwierdzenie rejestracji, logowanie (z wykorzystaniem sendgrida)

---
+++ UrbexerAPI/entities/BazaDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APIpz.entities
+{
+    public class BazaDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var serverVersion = new MariaDbServerVersion(new Version(10, 6));
+            optionsBuilder.UseMySql("server=localhost;port=3306;database=Unnamed;user=root;password=urbex", serverVersion);
+        }
+
+        public DbSet<Uzytkownik> Uzytkownicy { get; set; }
+        public DbSet<Miejsce> Miejsca { get; set; }
+        public DbSet<Opis> Opisy { get; set; }
+        public DbSet<Wyprawa> Wyprawy { get; set; }
+        public DbSet<Zdjecie> Zdjecia { get; set; }
+    }
+    //protected override void OnModelCreating(ModelBuilder modelbuilder)
+    //{
+       
+    //}
+}
