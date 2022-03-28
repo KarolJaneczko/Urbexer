096957c6451fc372fbbf5fd46dd7bed0f7ea8f30
Monika Banaszczak <m.banaszczak99@gmail.com>
2022-03-28 22:56:09 +0200

    Naprawa migracji do bazy

--- UrbexerAPI/Migrations/20220328205216_Odwiedzone.cs
+++ UrbexerAPI/Migrations/20220328205216_Odwiedzone.cs
@@ -1,32 +1,43 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace APIpz.Migrations
 {
-    public partial class dodanieOdwiedzone : Migration
+    public partial class Odwiedzone : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropForeignKey(
-                name: "FK_Miejsca_Uzytkownicy_UzytkownikId",
-                table: "Miejsca");
+                name: "FK_Miejsce_Uzytkownik_UzytkownikId",
+                table: "Miejsce");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Zdjecie_Uzytkownik_UzytkownikId",
+                table: "Zdjecie");
 
             migrationBuilder.DropIndex(
-                name: "IX_Miejsca_UzytkownikId",
-                table: "Miejsca");
+                name: "IX_Zdjecie_UzytkownikId",
+                table: "Zdjecie");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Miejsce_UzytkownikId",
+                table: "Miejsce");
 
             migrationBuilder.DropColumn(
                 name: "UzytkownikId",
-                table: "Miejsca");
+                table: "Zdjecie");
+
+            migrationBuilder.DropColumn(
+                name: "UzytkownikId",
+                table: "Miejsce");
 
             migrationBuilder.CreateTable(
                 name: "Odwiedzone",
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                        .Annotation("SqlServer:Identity", "1, 1"),
                     OdwiedzonePrzezId = table.Column<int>(type: "int", nullable: false),
                     OdwiedzonyUrbexId = table.Column<int>(type: "int", nullable: false)
                 },
@@ -34,19 +45,18 @@ namespace APIpz.Migrations
                 {
                     table.PrimaryKey("PK_Odwiedzone", x => x.Id);
                     table.ForeignKey(
-                        name: "FK_Odwiedzone_Miejsca_OdwiedzonyUrbexId",
+                        name: "FK_Odwiedzone_Miejsce_OdwiedzonyUrbexId",
                         column: x => x.OdwiedzonyUrbexId,
-                        principalTable: "Miejsca",
+                        principalTable: "Miejsce",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
                     table.ForeignKey(
-                        name: "FK_Odwiedzone_Uzytkownicy_OdwiedzonePrzezId",
+                        name: "FK_Odwiedzone_Uzytkownik_OdwiedzonePrzezId",
                         column: x => x.OdwiedzonePrzezId,
-                        principalTable: "Uzytkownicy",
+                        principalTable: "Uzytkownik",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
-                })
-                .Annotation("MySql:CharSet", "utf8mb4");
+                });
 
             migrationBuilder.CreateIndex(
                 name: "IX_Odwiedzone_OdwiedzonePrzezId",
@@ -66,20 +76,38 @@ namespace APIpz.Migrations
 
             migrationBuilder.AddColumn<int>(
                 name: "UzytkownikId",
-                table: "Miejsca",
+                table: "Zdjecie",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UzytkownikId",
+                table: "Miejsce",
                 type: "int",
                 nullable: true);
 
             migrationBuilder.CreateIndex(
-                name: "IX_Miejsca_UzytkownikId",
-                table: "Miejsca",
+                name: "IX_Zdjecie_UzytkownikId",
+                table: "Zdjecie",
+                column: "UzytkownikId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Miejsce_UzytkownikId",
+                table: "Miejsce",
                 column: "UzytkownikId");
 
             migrationBuilder.AddForeignKey(
-                name: "FK_Miejsca_Uzytkownicy_UzytkownikId",
-                table: "Miejsca",
+                name: "FK_Miejsce_Uzytkownik_UzytkownikId",
+                table: "Miejsce",
                 column: "UzytkownikId",
-                principalTable: "Uzytkownicy",
+                principalTable: "Uzytkownik",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Zdjecie_Uzytkownik_UzytkownikId",
+                table: "Zdjecie",
+                column: "UzytkownikId",
+                principalTable: "Uzytkownik",
                 principalColumn: "Id");
         }
     }
