ccb1de4b1a79e88f9e7998fb9f3dd4bdfa4f3c4c
MarcinWozniak345 <Marinox1234@wp.pl>
2022-03-29 17:02:23 +0200

    commit 29-03-2022

---
+++ UrbexerAPI/Models/PokazOpinieDoMiejscaDto.cs
@@ -0,0 +1,9 @@
+﻿namespace APIpz.Models
+{
+    public class PokazOpinieDoMiejscaDto
+    {
+        public string NazwaMiejsca { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
--- UrbexerAPI/Services/UrbexService.cs
+++ UrbexerAPI/Services/UrbexService.cs
@@ -2,8 +2,11 @@
 using APIpz.Exceptions;
 using APIpz.Middleware;
 using APIpz.Models;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace APIpz.Services
 {
@@ -12,6 +15,10 @@ namespace APIpz.Services
         List<Miejsce> GetAll();
 
         void DodajOdwiedzone(DodajOdwiedzoneDto dto);
+        PageResult<ZwracaneOdwiedzoneDto> PokazMojeOdwiedzone(PokazMojeOdwiedzoneDto dto);
+        PageResult<ZwracaneOdwiedzoneDto> PokazCzyjesOdwiedzone(PokazCzyjesOdwiedzoneDto dto);
+        void DodajOpinie(DodajOpinieDto dto);
+        PageResult<OpiniaDto> PokazOpinieDoMiejsca(PokazOpinieDoMiejscaDto dto);
 
     }
     public class UrbexService : IUrbexService
@@ -20,13 +27,14 @@ namespace APIpz.Services
         private readonly ILogger<ErrorHandlingMiddleware> _logger;
         private readonly IAuthorizationHandler _authorizationHandler;
         private readonly IUserContextService _userContextService;
-
-        public UrbexService(BazaDbContext context, ILogger<ErrorHandlingMiddleware> logger, IAuthorizationHandler authorizationHandler, IUserContextService userContextService)
+        private readonly IMapper _mapper;
+        public UrbexService(BazaDbContext context, ILogger<ErrorHandlingMiddleware> logger, IAuthorizationHandler authorizationHandler, IUserContextService userContextService, IMapper mapper)
         {
             _context = context;
             _logger = logger;
             _authorizationHandler = authorizationHandler;
             _userContextService = userContextService;
+            _mapper = mapper;
         }
 
         public List<Miejsce> GetAll()
@@ -51,5 +59,78 @@ namespace APIpz.Services
 
         }
 
+        public PageResult<ZwracaneOdwiedzoneDto> PokazMojeOdwiedzone(PokazMojeOdwiedzoneDto dto)
+        {
+            var zapytanie = _context.Odwiedzone
+                                            .Where(o => o.OdwiedzonePrzez.Id == (int)_userContextService.GetUserId)
+                                            .ToList();
+
+            var ListaOdwiedzonych = zapytanie
+                .Skip(dto.PageSize * (dto.PageNumber - 1))
+                .Take(dto.PageSize)
+                .ToList();
+
+            var totalitemsCount = zapytanie.Count();
+            var odwiedzoneDtos = _mapper.Map<List<ZwracaneOdwiedzoneDto>>(ListaOdwiedzonych);
+            var result = new PageResult<ZwracaneOdwiedzoneDto>(odwiedzoneDtos, totalitemsCount, dto.PageSize, dto.PageNumber);
+
+            return result;
+
+        }
+        public PageResult<ZwracaneOdwiedzoneDto> PokazCzyjesOdwiedzone(PokazCzyjesOdwiedzoneDto dto)
+        {
+            var zapytanie = _context.Odwiedzone
+                                            .Where(o => o.OdwiedzonePrzez.Login == dto.Login)
+                                            .ToList();
+
+            var ListaOdwiedzonych = zapytanie
+                .Skip(dto.PageSize * (dto.PageNumber - 1))
+                .Take(dto.PageSize)
+                .ToList();
+
+            var totalitemsCount = zapytanie.Count();
+            var odwiedzoneDtos = _mapper.Map<List<ZwracaneOdwiedzoneDto>>(ListaOdwiedzonych);
+            var result = new PageResult<ZwracaneOdwiedzoneDto>(odwiedzoneDtos, totalitemsCount, dto.PageSize, dto.PageNumber);
+
+            return result;
+            //return ListaOdwiedzonych;
+
+        }
+
+        public void DodajOpinie(DodajOpinieDto dto)
+        {
+            var odwiedzony = _context.Odwiedzone.
+                FirstOrDefault(o => o.OdwiedzonyUrbex.Nazwa == dto.Odwiedzony && o.OdwiedzonePrzez.Id == (int)_userContextService.GetUserId);
+            _context.Attach(odwiedzony);
+
+            var nowaOpinia = new Opinia()
+            {
+                Odwiedzony = odwiedzony,
+                Ocena = dto.Ocena,
+                Tekst = dto.Tekst
+            };
+            _context.Opinia.Add(nowaOpinia);
+            _context.SaveChanges();
+                              
+        }
+
+        public PageResult<OpiniaDto> PokazOpinieDoMiejsca(PokazOpinieDoMiejscaDto dto)
+        {
+            var zapytanie = _context.Opinia.Where(o => o.Odwiedzony.OdwiedzonyUrbex.Nazwa == dto.NazwaMiejsca);
+
+
+            var opinie = zapytanie
+                .Skip(dto.PageSize * (dto.PageNumber - 1))
+                .Take(dto.PageSize)
+                .ToList();
+
+            var totalitemsCount = zapytanie.Count();
+
+            var opinieDtos = _mapper.Map<List<OpiniaDto>>(opinie);
+
+            var result = new PageResult<OpiniaDto>(opinieDtos, totalitemsCount, dto.PageSize, dto.PageNumber);
+            return result;
+            
+        }
     }
 }
