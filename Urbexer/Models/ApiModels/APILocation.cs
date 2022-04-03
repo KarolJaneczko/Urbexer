e1520004dd62ace469cb34efb6ddfc0c546d0cd2
TheEggmo <48593109+TheEggmo@users.noreply.github.com>
2022-04-03 11:20:49 +0200

    Zmiana na netstandard2.1 i C#8.0
    Nestandard2.1 umożliwia korzystanie z C#8.0, który pozwala oznaczać pola jako nullable, co jest dosyć przydatne przy pracą z bazą danych imo

--- Urbexer/Models/ApiModels/APILocation.cs
+++ Urbexer/Models/ApiModels/APILocation.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using System.Text;
 
+#nullable enable
+
 namespace Urbexer.Models.ApiModels {
     public class APILocation {
         public int id;
-        public string nazwa;
-        //public string miejsce_Kategoria;
-        public string adres;
+        public string nazwa = "";
+        public string? miejsce_Kategoria;
+        public string? adres;
         public float wspolrzedneLAT;
         public float wspolrzedneLNG;
-        //public string zdjecie;
-        //public int trudnosc;
-        //public int doswiadczenie;
-        //public string link;
-
+        public string? zdjecie;
+        public int? trudnosc;
+        public int? doswiadczenie;
+        public string? link;
     }
 }
