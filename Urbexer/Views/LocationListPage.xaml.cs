9e42a3d449df15275c3c54d09e9e3cabe5108984
TheEggmo <48593109+TheEggmo@users.noreply.github.com>
2022-04-21 18:06:28 +0200

    Klikanie na karty lokacji w liście lokacji przenosi na stronę szczegółów
    Niestety jest to robione eventem zamiast komendą, bo nie wiem jak przekazać SelectedItem jako parametr

--- Urbexer/Views/LocationListPage.xaml.cs
+++ Urbexer/Views/LocationListPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using Urbexer.Models;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Urbexer.Views {
@@ -6,6 +7,16 @@ namespace Urbexer.Views {
     public partial class LocationListPage : ContentPage {
         public LocationListPage() {
             InitializeComponent();
+        }
+
+        private void CollectionView_SelectionChanged(CollectionView sender, SelectionChangedEventArgs e) {
+            // To można zrobić komendą, ale nie wiem jak przekazać SelectedItem jako parametr do komendy
+            Location location = (Location)sender.SelectedItem;
+            int id = location.Id;
+            if (location.Id < 0) return; // Id powinno być nieujemne.
+
+            var route = $"{nameof(LocationDetailsPage)}?LocationId={location.Id}";
+            Shell.Current.GoToAsync(route);
         }
     }
 }
