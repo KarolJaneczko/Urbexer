63b856e0bb89139454cebb893f8ccb31400f45bc
Monika Banaszczak <m.banaszczak99@gmail.com>
2022-05-03 09:56:02 +0200

    Naprawa polskich znaków i id w tabeli Miejsca

---
+++ UrbexerAPI/Skrypty/Polskie znaki i konwersja Miejsc.sql
@@ -0,0 +1,68 @@
+
+SET IDENTITY_INSERT [dbo].[Miejsce2] ON 
+GO
+
+/****** Script for SelectTopNRows command from SSMS  ******/
+Insert into Miejsce2 (Id, Nazwa, Miejsce_KategoriaId, Adres, WspolrzedneLAT, WspolrzedneLNG, DataDodania, Miasto,  WojewodztwoId, Opis)
+SELECT TOP (100) [id]
+      ,[Nazwa]
+      ,[Miejsce_Kategoria] as Miejsce_KartegoriaId
+      ,[Adres]
+      ,Convert(real, Replace([WspolrzedneLAT],',','.')) as WspolrzedneLAT
+      ,Convert(real, Replace([WspolrzedneLNG],',','.')) as WspolrzedneLNG
+      ,[DataDodania]
+      ,[Miasto]
+      ,[WojewodztwoId]
+      , 
+	  Replace(
+		Replace(
+			Replace(
+				Replace(
+					Replace(
+						Replace(
+							Replace(
+								Replace(
+									Replace(
+										Replace(
+											Replace(
+												Replace(
+													Replace(
+														Replace(
+															Replace(
+																Replace(
+																	Replace(
+																		Replace(
+																			Replace(
+																				Replace(
+																					Replace(
+																						Replace([Opis], '<p>', ''),
+																					'<br />', ''),
+																				'\xf3', 'Û'),
+																			'</p>', ''),
+																		'\u0105', 'π'),
+																	'\u0107', 'Ê'),
+																'\u0119', 'Í'),
+															'\u0142', '≥'),
+														'\u0144', 'Ò'),
+													'\u00f3', 'Û'),
+												'\u015b', 'ú'),
+											'\u017a', 'ü'),
+										'\u017c', 'ø'),
+									'\u0104', '•'),
+								'\u0106', '∆'),
+							'\u0118', ' '),
+						'\u0141', '£'),
+					'\u0143', '—'),
+				'\u00d3', '”'),
+			'\u015a', 'å'),
+		'\u0179', 'è'),
+	  '\u017b', 'Ø')
+	  
+
+	  as Opis
+  FROM [dbo].[TempMiejsca]
+
+GO
+
+SET IDENTITY_INSERT [dbo].[Miejsce2] OFF 
+GO
