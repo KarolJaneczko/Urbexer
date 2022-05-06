718241864c2d46f7c859d3f902ffdc8151a5ebea
TheEggmo <48593109+TheEggmo@users.noreply.github.com>
2022-05-06 21:23:39 +0200

    Update APIReview.cs
    Dokumentacja

--- Urbexer/Models/ApiModels/APIReview.cs
+++ Urbexer/Models/ApiModels/APIReview.cs
@@ -2,10 +2,18 @@
 #nullable enable
 
 namespace Urbexer.Models.ApiModels {
+    /// <summary>
+    /// Recenzja pobrana z bazy.
+    /// </summary>
     public class APIReview {
+        /// <summary>
+        /// Nazwa użytkownika.
+        /// </summary>
         public string? ktoOdwiedzil;
-        public string? miejsce;
         public int ocena;
+        /// <summary>
+        /// Treść recenzji.
+        /// </summary>
         public string? tekst;
     }
 }
