d62ea0c68d71cc75d7deff552c99d4516e4e8de9
TheEggmo <48593109+TheEggmo@users.noreply.github.com>
2022-05-06 20:31:48 +0200

    Update CustomMap.cs
    Dokumentacja

--- Urbexer/Controls/CustomMap.cs
+++ Urbexer/Controls/CustomMap.cs
@@ -5,6 +5,9 @@ using Urbexer.Models;
 using Xamarin.Forms.Maps;
 
 namespace Urbexer.Controls {
+    /// <summary>
+    /// Mapa służąca do przechowywania LocationPin i wyświetlania customowych grafik pinezek
+    /// </summary>
     public class CustomMap : Map{
         public List<LocationPin> LocationPins { get; set; }
         public CustomMap(MapSpan mapSpan) : base(mapSpan) { }
