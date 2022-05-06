bcb013ef18e88ed12e3c81051552c00c9513c73d
TheEggmo <48593109+TheEggmo@users.noreply.github.com>
2022-05-06 21:20:59 +0200

    Update APILocation.cs
    Dokumentacja i drobne poprawki

--- Urbexer/Models/ApiModels/APILocation.cs
+++ Urbexer/Models/ApiModels/APILocation.cs
@@ -2,18 +2,21 @@
 #nullable enable
 
 namespace Urbexer.Models.ApiModels {
+    /// <summary>
+    /// Klasa reprezentująca lokację pobraną z bazy.
+    /// </summary>
     public class APILocation {
         public int id;
         public string nazwa = "";
         public string? adres;
+        /// <summary>
+        /// Opis jest wyświetlany na stronie <see cref="Views.LocationDetailsPage"/>
+        /// </summary>
         public string opis = "";
         public int kategoriaId;
         public int wojewodztwoId;
         public float wspolrzedneLAT;
         public float wspolrzedneLNG;
-        public int? trudnosc;
-        public int? doswiadczenie;
-        public string? link;
         public APIPhoto[]? zdjecia;
     }
 }
