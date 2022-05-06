84f513fcaeb086cfcc826a5366f207bee2e5f9a0
TheEggmo <48593109+TheEggmo@users.noreply.github.com>
2022-05-06 21:16:45 +0200

    Update APIPhoto.cs
    Dokumentacja

--- Urbexer/Models/ApiModels/APIPhoto.cs
+++ Urbexer/Models/ApiModels/APIPhoto.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Urbexer.Models.ApiModels {
+    /// <summary>
+    /// Klasa reprezentująca zdjęcie pobrane z bazy <para/>
+    /// Ze strony front-endu wykorzystywany jest tylko <see cref="link"/>
+    /// </summary>
     public class APIPhoto {
         public int id;
         public string miejsce;
@@ -10,6 +14,9 @@ namespace Urbexer.Models.ApiModels {
         public string rozmiar;
         public int wysokosc;
         public int szerokosc;
+        /// <summary>
+        /// Link html wskazujący na dane zdjęcie.
+        /// </summary>
         public string link;
     }
 }
