957e0c841cf261241ae765355c4e44787a525462
Karol Janeczko <karol.janeczko@outlook.com>
2022-05-07 15:44:38 +0200

    klauzula i regulamin

--- Urbexer/Views/OtherViews/InfoPage.xaml.cs
+++ Urbexer/Views/OtherViews/InfoPage.xaml.cs
@@ -1,11 +1,16 @@
-﻿using Urbexer.ViewModels;
+﻿using System;
+using Urbexer.ViewModels;
 using Xamarin.Forms;
 
 namespace Urbexer.Views {
     public partial class InfoPage : ContentPage {
         public InfoPage() {
+            Routing.RegisterRoute(nameof(PolicyPage), typeof(PolicyPage));
             InitializeComponent();
             BindingContext = new HomeViewModel();
+        }
+        public async void ShowPolicy(object sender, EventArgs e) {
+            await Shell.Current.GoToAsync(nameof(PolicyPage));
         }
     }
 }
