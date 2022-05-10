029622b539d748aed54d4ba16ae44d98732ac74f
MarcinWozniak345 <Marinox1234@wp.pl>
2022-05-10 13:34:48 +0200

    dodanie metody ileLokacjiDanejKategoriUzytkownikOdwiedzil

---
+++ UrbexerAPI/Models/IleLokacjiDanejKategoriUzytkownikOdwiedziDto.cs
@@ -0,0 +1,8 @@
+﻿namespace APIpz.Models
+{
+    public class IleLokacjiDanejKategoriUzytkownikOdwiedziDto
+    {
+        public string Login { get; set; }
+        public int KategoriaId { get; set; }
+    }
+}
