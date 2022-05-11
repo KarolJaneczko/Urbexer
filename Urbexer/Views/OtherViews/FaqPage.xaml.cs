982104d6e3bdf3e68e81cade8f5c43298751e227
Karol Janeczko <karol.janeczko@outlook.com>
2022-05-11 22:00:29 +0200

    interpunkcja + przeredagowanie

--- Urbexer/Views/OtherViews/FaqPage.xaml.cs
+++ Urbexer/Views/OtherViews/FaqPage.xaml.cs
@@ -30,13 +30,13 @@ namespace Urbexer.Views {
             InitializeComponent();
             BindingContext = this;
             Questions = new ObservableCollection<Question>() {
-                new Question{Id = 1 , Title= "Czy Aplikacja jest darmowa?", BodyDescription = "Tak, URBEXER jest w pełni darmową aplikacją i nie ma wersji premium"},
-                new Question{Id = 2 , Title= "Czy do korzystania z aplikacji jest potrzebny internet?", BodyDescription = "Tak, aby korzystać z aplikacji mieć włączony internet w telefonie. "},
-                new Question{Id = 3 , Title= "Czym wyróżnia się aplikacja URBEXER?", BodyDescription = "Dzięki współpracy z serwisem urbexy.pl aplikacja na dzień premiery zawiera ponad 2000 udostępnionych lokacji na terenie całego kraju. Aplikacja pozwala również rywalizować z innymi użytkownikami w liczbie odwiedzonych miejsc. Stworzyliśmy dopracowane rankingi podzielone na poszczególne kategorie lokacji udostępnionych w aplikacji."},
-                new Question{Id = 4 , Title= "Jakie kategorie rankingowe znajdą się w aplikacji?",BodyDescription = "Ranking jest podzielony na 12 głownych kategorii, jedną przydzieloną dla pozostałych oraz jedną przeznaczoną na sumaryczny wynik odwiedzonych miejsc"},
-                new Question{Id = 5 , Title= "Czy można dodać swoje lokacje?", BodyDescription = "Jeśli ktoś chce dodać miejsc, które jest warte odwiedzenia wystarczy zgłosić je na..."},
-                new Question{Id = 6 , Title= "Jak zgłosić błąd w Aplikacji",BodyDescription = "Wystarczy wybrać w menu po lewej stronie aplikacji opcję  KONTAKT. Po kliknięciu w polu tekstowym można opisać problem. Alternatywnie można się skontaktować bezpośrednio na adres mailowy: urbexer@gmail.com "},
-                new Question{Id = 7 , Title= "Na jakich urzędzeniach jest dostępna aplikacja?",BodyDescription = "Aplikacja jest dostępna na urządzenia z sytemem Android  w wersji 4.1 oraz nowszych."},
+                new Question{Id = 1 , Title= "Czy Aplikacja jest darmowa?", BodyDescription = "Tak, URBEXER jest w pełni darmową aplikacją i nie ma wersji premium."},
+                new Question{Id = 2 , Title= "Czy do korzystania z aplikacji jest potrzebny internet?", BodyDescription = "Tak, aby korzystać z aplikacji należy posiadać stałe połączenie z internetem w telefonie."},
+                new Question{Id = 3 , Title= "Czym wyróżnia się aplikacja URBEXER?", BodyDescription = "Dzięki współpracy z serwisem Urbexy.pl aplikacja na dzień premiery zawiera ponad 2000 udostępnionych lokacji na terenie całego kraju. Aplikacja pozwala również rywalizować z innymi użytkownikami w liczbie odwiedzonych miejsc. Stworzyliśmy dopracowane rankingi podzielone na poszczególne kategorie lokacji udostępnionych w aplikacji."},
+                new Question{Id = 4 , Title= "Jakie kategorie rankingowe znajdą się w aplikacji?",BodyDescription = "Ranking jest podzielony na 12 głównych kategorii, jedną przydzieloną dla pozostałych oraz jedną przeznaczoną na sumaryczny wynik odwiedzonych miejsc."},
+                new Question{Id = 5 , Title= "Czy można dodać swoje lokacje?", BodyDescription = "Jeśli ktoś chce dodać miejsce, które jest warte odwiedzenia wystarczy zgłosić je na adres urbexer@outlook.com"},
+                new Question{Id = 6 , Title= "Jak zgłosić błąd w Aplikacji",BodyDescription = "Wystarczy wybrać w menu po lewej stronie aplikacji opcję KONTAKT. Po kliknięciu w polu tekstowym można opisać problem. Alternatywnie można się skontaktować bezpośrednio na adres mailowy: urbexer@outlook.com "},
+                new Question{Id = 7 , Title= "Na jakich urządzeniach jest dostępna aplikacja?",BodyDescription = "Aplikacja jest dostępna, tylko i wyłącznie, na urządzenia z sytemem Android w wersji 4.1 oraz nowszych."},
 
             };
         }
